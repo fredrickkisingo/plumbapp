5c9ba4500cc9043c31ddc94e4548e2445e3fbfb2
fred <fredrickkisingo@gmail.com>
2021-03-02 00:44:06 +0300

    addition of sweet alert

--- app/Http/Controllers/Admin/AboutusController.php
+++ app/Http/Controllers/Admin/AboutusController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Session;
 use App\Models\Abouts;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -23,6 +24,8 @@ class AboutusController extends Controller
         $aboutus->description= $request->input('description');
         
         $aboutus->save();
+
+        Session::flash('statuscode','success');
         return redirect('/abouts')->with('status','Data added for About Us');
 
     }
@@ -40,6 +43,7 @@ class AboutusController extends Controller
         $aboutus->description= $request->input('description');
         $aboutus->update();
 
+        Session::flash('statuscode','info');
         return redirect('abouts')->with('status','Your Aboutus Details have been  updated!');
 
     }
@@ -48,6 +52,7 @@ class AboutusController extends Controller
         $aboutus= Abouts::findOrFail($id);
         $aboutus->delete();
             
+        Session::flash('statuscode','error');
         return redirect('abouts')->with('status','Your Aboutus detail has been  deleted!');
             
      }     
