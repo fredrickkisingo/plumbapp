a3c3e69b323f836957b6740371a24ec3d3eae38e
fred <fredrickkisingo@gmail.com>
2021-03-02 00:47:29 +0300

    addition of slider images

---
+++ resources/views/inc/slider.blade.php
@@ -0,0 +1,21 @@
+<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-interval="10000">
+            <img src="/storage/sliders/plumber3.jpg" class="d-block w-100" alt="Slider 1">
+        </div>
+        <div class="carousel-item" data-interval="2000">
+            <img src="/storage/sliders/carpenter2.jpg" class="d-block w-100" alt="Slider 2">
+        </div>
+        <div class="carousel-item">
+            <img src="/storage/sliders/electrician.jpg" class="d-block w-100" alt="Slider 3">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
