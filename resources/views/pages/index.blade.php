5676dd5f2bd80aea42b036f6515d4eee516b441a
fred <fredrickkisingo@gmail.com>
2021-03-03 18:38:37 +0300

    addition of procfile from heroku

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,6 +6,6 @@
        <p><strong> This a site for you to find potential clients as an artisan and customers to find nearby artisans near them.</strong></p>
        <footer>
               <p>Copyright &copy;2021 Fredrick Kisingo</p>
-            </footer>
+       </footer>
 </div>       
 @endsection
