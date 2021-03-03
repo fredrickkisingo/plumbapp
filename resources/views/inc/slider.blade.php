86380b19b9e6c1947de40f377f3cff4dad26faff
fred <fredrickkisingo@gmail.com>
2021-03-03 18:48:32 +0300

    addition of file path from the sliders

--- resources/views/inc/slider.blade.php
+++ resources/views/inc/slider.blade.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-interval="2000">
-            <img src="/storage/sliders/plumber3.jpg" class="d-block w-100" style="max-width: 100%;height:380px" alt="Slider 1">
+            <img src="public/storage/sliders/plumber3.jpg" class="d-block w-100" style="max-width: 100%;height:380px" alt="Slider 1">
         </div>
         <div class="carousel-item" data-interval="2000">
-            <img src="/storage/sliders/carpenter2.jpg" class="d-block w-100" style="max-width: 100%;height:380px" alt="Slider 2">
+            <img src="public/storage/sliders/carpenter2.jpg" class="d-block w-100" style="max-width: 100%;height:380px" alt="Slider 2">
         </div>
         <div class="carousel-item">
-            <img src="/storage/sliders/electrician.jpg" class="d-block w-100"style="max-width: 100%;height:380px" alt="Slider 3">
+            <img src="public/storage/sliders/electrician.jpg" class="d-block w-100"style="max-width: 100%;height:380px" alt="Slider 3">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
