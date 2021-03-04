44b5d16d21dc9c632fe2e2d7f90709bff6686f3e
fred <fredrickkisingo@gmail.com>
2021-03-04 18:29:01 +0300

    changes to the about description

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,5 +3,6 @@
        <h1>This is PLUMBAPP'S official about page</h1>
        <h2>This is a site for you as a home owner to get all your skilled personnel under one roof</h2>
             <p>With PLUMBAPP as a customer you get to find  the nearest artisan for your home repairs and  get that quality repair done.
-                  For artisans and craftsmen this is a web application for you to find potential clients and customers from around your area and even far and wide..possibilities are endless !</p>
+                  For artisans and craftsmen this is a web application for you to find potential clients and customers from around your area and even far and wide..possibilities are endless !
+            </p>
        @endsection
