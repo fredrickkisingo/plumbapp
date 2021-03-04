bcb1529add963400c8dadf6d0913829fdfa8a379
fred <fredrickkisingo@gmail.com>
2021-03-04 16:02:34 +0300

    fixing of the about information

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('content')
        <h1>This is PLUMBAPP'S official about page</h1>
-       <h2>This is for testing many things among them being about our site</h2>
- @endsection
+       <h2>This is a site for you as a home owner to get all your skilled personnel under one roof</h2>
+            <p>With PLUMBAPP as a customer you get to find  the nearest artisan for your home repairs and  get that quality repair done.
+                  For artisans and craftsmen this is a web application for you to find potential clients and customers from around your area and even far and wide..possibilities are endless !</p>
+       @endsection
