3e5bf101e9fb20d5e2f8d8b9c959f9ad6db3c691
fred <fredrickkisingo@gmail.com>
2021-03-11 15:19:10 +0300

    addition of user guides to the about us page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,10 @@
 @section('content')
        <h1>This is PLUMBAPP'S official about page</h1>
        <h2>This is a site for you as a home owner to get all your skilled personnel under one roof</h2>
-            <p>With PLUMBAPP as a customer you get to find  the nearest artisan for your home repairs and  get that quality repair done.
+            <p>1.With PLUMBAPP as a customer you get to find  the nearest artisan for your home repairs and  get that quality repair done.
                   For artisans and craftsmen this is a web application for you to find potential clients and customers from around your area and even far and wide..possibilities are endless !
             </p>
+            <p>2.For a new user you are required to register an account with PLUMBAPP then choose the role you want to sign up with either an artisan or customer</p>
+            <p>3.A customer will be able to view for available artisans of his/her choice depending on what he wants to be done then contact him using the contact details provided by the artisan</p>
+            <p>4.As an artisan registered with PLUMBAPP  you will post you job description together with the rates you charge and also location and contact details</p>
        @endsection
