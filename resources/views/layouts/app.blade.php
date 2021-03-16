ebf48e75202f81e6913d21f44ce4dcf8a36f76a4
fred <fredrickkisingo@gmail.com>
2021-03-16 17:24:07 +0300

    addition of favicon code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name','PLUMBAPP')}}</title>  
         
          <!-- Styles -->
