72ac32503ebab53b60f48070042fdaa759503d39
fred <fredrickkisingo@gmail.com>
2021-03-18 16:09:57 +0300

    spacing

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Auth::routes();
-
-
 Route::get('/about',function(){
     return view('pages.about');
 });
